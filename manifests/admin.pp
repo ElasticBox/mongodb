e9b4e30af90bee61ce2c9a66c324c49f34b77bbe
Arnaud Bonnet <arnaud@elasticbox.com>
2014-06-18 13:05:09 -0700

    Wait for Mongo to be available after restarting

--- manifests/admin.pp
+++ manifests/admin.pp
@@ -9,14 +9,6 @@ define mongodb::admin(
   
   include 'mongodb::params'
   
-  $wait = "mongo admin --eval \"db.users.find()\"; while [ $? -ne 0 ];do sleep 5;mongo admin --eval \"db.users.find()\";done"
-  exec { "${name}_wait_connection": 
-    command   => $wait,
-    path      => "/usr/bin:/usr/sbin:/bin:/sbin",
-    logoutput => true,
-    timeout   => 300,
-  }
-  
   $noauth = "mongo admin --eval \"db.createUser({user: \\\"${name}\\\",pwd: \\\"${password}\\\",roles:[{role: \\\"root\\\",db: \\\"admin\\\"},{role: \\\"restore\\\",db: \\\"admin\\\"}]})\""
   $auth = "if [ $? -eq 252 ]; then mongo -u ${admin_username} -p ${admin_password} admin --eval \"db.createUser({user: \\\"${name}\\\",pwd: \\\"${password}\\\",roles:[{role: \\\"userAdminAnyDatabase\\\",db: \\\"admin\\\"}]})\";fi"
   $created = "if [ $? -eq 252 ]; then echo 'User already created.';fi"
@@ -26,6 +18,5 @@ define mongodb::admin(
     command   => $command,
     path      => "/usr/bin:/usr/sbin:/bin:/sbin",
     logoutput => true,
-    require   => Exec["${name}_wait_connection"],
   }
 }
